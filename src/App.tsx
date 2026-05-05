dbb3f636161759c9e1741017a210914be118a2ce
Zozo <kozmazoltan144@gmail.com>
2026-05-05 20:08:33 +0200

    Removed BL popup window

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import { Analytics } from "@vercel/analytics/react";
-import ChampionsLeagueModal from "./components/ChampionsLeagueModal";
 function App() {
   return (
     <>
       <Navbar />
-      <ChampionsLeagueModal />
       <Home />
       <Analytics />
     </>
